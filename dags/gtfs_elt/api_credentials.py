3ca153400415ad761d76b00e26087e2e4d4a1605
graffignabracco <juanalbertograffigna@gmail.com>
2022-01-31 01:11:21 -0300

    Credentials Hidden

--- dags/gtfs_elt/api_credentials.py
+++ dags/gtfs_elt/api_credentials.py
@@ -1,4 +1,4 @@
 credentials = {
-    'CLIENT_ID': '3e72d2097b8840c88e8cf32b88755e23',
-    'CLIENT_SECRET': 'cA3c7C7F9d894eD4AC77da761287d70E'
+    'CLIENT_ID': '***',
+    'CLIENT_SECRET': '***'
 }
